954c58f1761c5a484855602b540ff183d1aec81a
cldborges <cldborges@yahoo.com.br>
2022-04-13 10:40:05 -0300

    retirado 5m

--- main.py
+++ main.py
@@ -5,19 +5,13 @@ chrome_path = 'C:/PortableApps/PortableApps/GoogleChromePortable/App/Chrome-bin/
 webbrowser.get(chrome_path).open('http://docs.python.org/')
 
 caminho = os.path.normpath("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
-print(caminho)
 os.startfile(caminho)'''
 
 #os.startfile("C:\Users\z0026jjc\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe")
 #caminho = os.path.normpath (r'C:\Users\z0026jjc\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe")')
-#print(caminho)
 
-
-
-os.startfile("outlook")
 os.system('C:/Users/z0026jjc/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
-for caminho in r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe":
-#for caminho in r"C:\Users\z0026jjc\OneDrive - Atos\5m.exe", r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe":
+for caminho in r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe", "outlook":
 #for caminho in r"C:\Users\z0026jjc\OneDrive - Atos\5m.exe", r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe":
     os.path.normpath(caminho)
     os.startfile(caminho)
