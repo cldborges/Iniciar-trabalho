8a33c9f55060aadc7b65f89ec87260c11d8af916
cldborges <cldborges@yahoo.com.br>
2023-12-12 11:15:15 -0300

    fix novo teams

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ os.startfile(caminho)'''
 #os.startfile("C:\Users\z0026jjc\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe")
 #caminho = os.path.normpath (r'C:\Users\z0026jjc\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe")')
 
-os.system('C:/Users/z0026jjc/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
-for caminho in r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe", "outlook":
+#teams antigo
+# os.system('C:/Users/z0026jjc/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
+
+for caminho in r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe", "outlook", "C:\Program Files\WindowsApps\MSTeams_23306.3315.2560.6525_x64__8wekyb3d8bbwe\ms-teams.exe":
 #for caminho in r"C:\Users\z0026jjc\OneDrive - Atos\5m.exe", r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe":
     os.path.normpath(caminho)
     os.startfile(caminho)
