8586d2450214da4a4ef64647d569104889f51fb8
cldborges <cldborges@yahoo.com.br>
2024-11-11 09:59:28 -0300

    add msg tomar remédios

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+import easygui
 
 '''import webbrowser
 chrome_path = 'C:/PortableApps/PortableApps/GoogleChromePortable/App/Chrome-bin/chrome.exe %s'
@@ -12,6 +14,13 @@ os.startfile(caminho)'''
 
 #teams antigo
 # os.system('C:/Users/z0026jjc/AppData/Local/Microsoft/Teams/Update.exe --processStart "Teams.exe"')
+
+# Obtém o horário atual
+hora_atual = datetime.datetime.now().hour
+
+# Verifica se a hora está entre 07h e 09h
+if 7 <= hora_atual < 9:
+    easygui.msgbox('Tomar os remédios!!')
 
 for caminho in r"C:\PortableApps\PortableApps\GoogleChromePortable\GoogleChromePortable.exe", "outlook", "C:\Program Files\WindowsApps\MSTeams_23306.3315.2560.6525_x64__8wekyb3d8bbwe\ms-teams.exe":
 #for caminho in r"C:\Users\z0026jjc\OneDrive - Atos\5m.exe", r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe":
